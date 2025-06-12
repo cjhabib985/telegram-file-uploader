ff1725d51a98bcd7a7eda94330636965d04dc30e
cjhabib985 <davidcj835@gmail.com>
2025-06-12 08:50:19 +0330

    Update main.py
    ok

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def handle_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 # ✅ بدون ایمپورت جداگانه از filters.py
-file_filter = filters.Document.ALL | filters.Video | filters.Audio
+file_filter = filters.Document.ALL | filters.VIDEO | filters.Audio
 
 app.add_handler(MessageHandler(file_filter, handle_file))
 app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'^/start'), handle_start))
